0c6413c33d6045a0bd4a6686f6e0ed9482b10a96
Shannon Barrett <sbarrett00@live.com>
2016-10-13 11:30:22 -0500

    Adding TextPad

---
+++ TextPad/tools/chocolateyinstall.ps1
@@ -0,0 +1,54 @@
+﻿
+$ErrorActionPreference = 'Stop';
+
+
+$packageName= 'TextPad'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://textpad.com/download/v80/win32/txpeng802-32.zip'
+$url64      = 'http://textpad.com/download/v80/x64/txpeng802-64.zip'
+$downloadedZip = Join-Path $toolsDir 'textpad.zip'
+$fileLocation = Join-Path $toolsDir 'setup.exe'
+
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir  
+  url           = $url
+  url64bit      = $url64
+  file         = $fileLocation
+  filefullpath  = $downloadedZip
+
+  softwareName  = 'TextPad*'
+
+  checksum      = '448AB57625EA1B04911DFAACEC86DFBA'
+  checksumType  = 'md5'
+  checksum64    = 'F9471BD53B2987267F45AD5768B4E2BC'
+  checksumType64= 'md5'
+
+  silentArgs   = '/s /v""/qn""'
+}
+
+
+Get-ChocolateyWebFile $packageName $downloadedZip $url $url64
+
+
+Get-ChocolateyUnzip -FileFullPath $downloadedZip -Destination $toolsDir
+
+$installerArgs  = @{
+  packageName   = $packageName
+  silentArgs    = '/S /v""/qn""'
+  file          = $fileLocation
+  fileType      = 'exe'
+}
+
+Install-ChocolateyInstallPackage @installerArgs
+
+
+
+
+
+
+
+
+
+
