f3b6f81d9ff1951963293602dd13a3863658c435
Shannon Barrett <shannon@SimpleLTC.com>
2017-05-01 11:21:07 -0500

    Added Redis Desktop Manager

---
+++ RedisDesktopManager/tools/chocolateyinstall.ps1
@@ -0,0 +1,27 @@
+﻿
+$ErrorActionPreference = 'Stop';
+
+
+$packageName= 'redis-desktop-manager'
+$url64        = 'https://github.com/uglide/RedisDesktopManager/releases/download/0.8.8/redis-desktop-manager-0.8.8.384.exe'
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'exe'
+  url64bit      = $url64
+  silentArgs    = '/S /v""/qn""'
+  checksum64    = '3CD608D52BF91991DF25678023AAFC34F1237865863E2E71A050AF78DC520794'
+  checksumType64= 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
