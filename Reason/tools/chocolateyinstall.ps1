dd9f3c098261b47d48b98f278b33204c2027cc91
Shannon Barrett <sbarrett00@live.com>
2020-05-06 17:05:21 -0500

    Added reason package

---
+++ Reason/tools/chocolateyinstall.ps1
@@ -0,0 +1,33 @@
+﻿
+$ErrorActionPreference = 'Stop'; # stop on all errors
+
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://cdn.propellerheads.se/Reason10/Reason_1040_d4.zip' 
+$downloadedZip = Join-Path $toolsDir 'Reason_1040_d4.zip'
+$fileLocation = Join-Path $toolsDir 'Install Reason 10.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  filefullpath  = $downloadedZip
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url  
+  softwareName  = 'Reason' 
+  checksum      = 'D2619016AFD7901E3931ECDF98C0D376E76B4FCCE516962B34E67CD87483AC37'
+  checksumType  = 'sha256'  
+  validExitCodes= @(0) 
+}
+
+Get-ChocolateyWebFile @packageArgs
+
+Get-ChocolateyUnzip -FileFullPath $downloadedZip -Destination $toolsDir
+
+$installerArgs  = @{
+  packageName   = $packageName  
+  silentArgs   = '/LANG=en /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  file          = $fileLocation
+  fileType      = 'exe'
+}
+
+Install-ChocolateyInstallPackage @installerArgs
+
