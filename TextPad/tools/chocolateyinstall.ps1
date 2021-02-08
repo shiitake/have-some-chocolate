8e69d4db3a68ad5824845c7132de7f2bfd203cfb
Ross Smith II <ross@smithii.com>
2021-02-07 19:56:03 -0800

    Bump textpad to 8.5.1

--- TextPad/tools/chocolateyinstall.ps1
+++ TextPad/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $ErrorActionPreference = 'Stop';
 
 $packageName= 'TextPad'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://www.textpad.com/file?path=v85/win32/txpeng850-32.zip'
-$url64      = 'https://www.textpad.com/file?path=v85/x64/txpeng850-64.zip'
+$url        = 'https://www.textpad.com/file?path=v85/win32/txpeng851-32.zip'
+$url64      = 'https://www.textpad.com/file?path=v85/x64/txpeng851-64.zip'
 $downloadedZip = Join-Path $toolsDir 'textpad.zip'
 $fileLocation = Join-Path $toolsDir 'setup.exe'
 
@@ -14,9 +14,9 @@ $packageArgs = @{
   filefullpath  = $downloadedZip
   url           = $url
   url64bit      = $url64
-  checksum      = '378AD3E366760C3B1C16B370C28BB14B9535D49E0EBF5385B9F1A61BA3DD3278'
+  checksum      = 'BEB7B84252D025FE34DEBCDF55528868013DA4B60D4D91785BD2186F83EACFF1'
   checksumType  = 'sha256'
-  checksum64    = '9494F3746CA1CF0A98C2CD6E75D0B181FDA41ED9400C7B75BBB2F9C3A1BC5822'
+  checksum64    = 'BD5EE4F4B709FE2CC5135F74EE41E9FF0D65DB513D033F1DFEBB53A6E48F52AC'
   checksumType64= 'sha256'
 }
 
