cde6f60c39e619ecd3afc8047e2b9c8ece04734a
Aleksandr Chebotov <v-aleche@microsoft.com>
2021-06-23 17:44:25 +0300

    updated sql package to 18.7.1

--- sqlpackage/tools/chocolateyinstall.ps1
+++ sqlpackage/tools/chocolateyinstall.ps1
@@ -2,13 +2,13 @@
 $ErrorActionPreference = 'Stop';
 $packageName = 'SqlPackage'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64        = 'https://go.microsoft.com/fwlink/?linkid=2157302'
+$url64        = 'https://go.microsoft.com/fwlink/?linkid=2165212'
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir  
   url64bit      = $url64
   softwareName  = 'sqlpackage*'
-  checksum64    = 'F29BA3FBFD3095F0E71B72317177CA275E3EC466075A99229297863F840B5501'
+  checksum64    = '4CBED999ED26733A9FA55B2307EC4E505EA7094F73CA1591B2233BC017A9699E'
   checksumType64= 'sha256'
 }
 
