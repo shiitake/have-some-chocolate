7ef5f60d025d613a03ac64f7b38c41da0fc8b026
Shannon Barrett <sbarrett00@live.com>
2022-02-14 14:47:51 -0600

    Updated sqlpackage

--- sqlpackage/tools/chocolateyinstall.ps1
+++ sqlpackage/tools/chocolateyinstall.ps1
@@ -2,13 +2,13 @@
 $ErrorActionPreference = 'Stop';
 $packageName = 'SqlPackage'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64        = 'https://go.microsoft.com/fwlink/?linkid=2165007'
+$url64        = 'https://go.microsoft.com/fwlink/?linkid=2185669'
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir  
   url64bit      = $url64
   softwareName  = 'sqlpackage*'
-  checksum64    = '37E33BC9668A922AE29AC60AE501F4BC7C2EC20A8882B54917AE388F156CCB82'
+  checksum64    = '3953CD0CCA5218D9EA8BECE9D56B05A7188D68DB8FE403C6AE60D8E04DB40870'
   checksumType64= 'sha256'
 }
 
