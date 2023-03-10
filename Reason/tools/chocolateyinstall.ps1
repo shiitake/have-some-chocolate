5d8c57aa0f7d832b479f4f39926c3d34f67f08e0
shiitake <shannon@punq.io>
2023-03-10 16:14:59 -0600

    Added v12.5.1

--- Reason/tools/chocolateyinstall.ps1
+++ Reason/tools/chocolateyinstall.ps1
@@ -2,9 +2,9 @@
 $ErrorActionPreference = 'Stop'; # stop on all errors
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://a.phcdn.se/update/Stable/Reason_1135_d22-Stable-142-Win.zip' 
-$downloadedZip = Join-Path $toolsDir 'Reason_1135_d22-Stable-142-Win.zip'
-$fileLocation = Join-Path $toolsDir 'Install Reason 11.exe'
+$url        = 'https://a.phcdn.se/update/Stable/Reason_1251_d41-Stable-562-Win.zip' 
+$downloadedZip = Join-Path $toolsDir 'Reason_1251_d41-Stable-562-Win.zip'
+$fileLocation = Join-Path $toolsDir 'Install Reason 12.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -13,7 +13,7 @@ $packageArgs = @{
   fileType      = 'exe'
   url           = $url  
   softwareName  = 'Reason' 
-  checksum      = '631B872766B7A5C6BC41EF530B2764AE30179334D345290A5EA3C878AA870981'
+  checksum      = '3BE911D32F0337A4A0CA6C63F74959671E9E81926A7577AD25972AAF879DEE7B'
   checksumType  = 'sha256'  
   validExitCodes= @(0) 
 }
