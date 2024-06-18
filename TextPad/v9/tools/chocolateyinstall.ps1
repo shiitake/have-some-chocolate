539596115bf910e5f594f7a1d6a2c823299d4489
shiitake <shannon@punq.io>
2024-06-18 11:30:39 -0500

    updated headers

--- TextPad/v9/tools/chocolateyinstall.ps1
+++ TextPad/v9/tools/chocolateyinstall.ps1
@@ -30,7 +30,18 @@ $headers = @{
   "User-Agent" = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"
     "Accept" = "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8"
     "Content-Type" = "application/x-www-form-urlencoded"
-    "Cache-Control" = "max-age=0"
+    "Cache-Control" = max-age=0
+    "Host" = www.textpad.com
+    "Origin" = https://www.textpad.com
+    "Referer" = $url64
+    "Sec-Ch-Ua" = "Not/A)Brand";v="8", "Chromium";v="126", "Brave";v="126"
+    "Sec-Ch-Ua-Mobile" = ?0
+    "Sec-Ch-Ua-Platform" = "Windows"
+    "Sec-Fetch-Dest" = document
+    "Sec-Fetch-Mode" = navigate
+    "Sec-Fetch-Site" = same-origin
+    "Sec-Fetch-User" = ?1
+    "Sec-Gpc" = 1
 }
 
 $response = Invoke-WebRequest -Uri $downloadUrl -Method POST -Body $formData -OutFile $fileLocation -WebSession $webSession -Headers $headers
